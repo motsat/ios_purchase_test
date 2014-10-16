d08924340cbaea8e8037c01651049a51b25d1dd6
motsat <konpeiex@gmail.com>
2014-10-17 07:47:31 +0900

    initial commit

---
+++ SinglePurchaseItem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SinglePurchaseItem
+//
+//  Created by 佐藤 元紀 on 2014/10/17.
+//  Copyright (c) 2014年 佐藤 元紀. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SinglePurchaseItem/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SinglePurchaseItem
+//
+//  Created by 佐藤 元紀 on 2014/10/17.
+//  Copyright (c) 2014年 佐藤 元紀. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
